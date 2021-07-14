1fd8ea8d42a708c2e7f9c1d7fd0ce8047fae1182
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2021-07-14 22:28:49 +0530

    Heaps Using Priority Queue STL

---
+++ 11_Priority_Queue_And_Heap/02_Custom_Compare_PQ.cc
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<string>
+#include<climits>
+#include<algorithm>
+#include<array>
+#include<bitset>
+#include<deque>
+#include<list>
+#include<map>
+#include<queue>
+#include<set>
+#include<deque>
+#include<stack>
+#include<unordered_map>
+#include<unordered_set>
+#include<vector>
+#define ll long long int
+#define ld long long double
+#define PB push_back
+#define POB pop_back
+#define MP make_pair
+#define F first
+#define S second
+#define nl '\n'
+#define tab '\t'
+#define pi pair<int, int>
+#define vi vector<int>
+#define vs vector<string>
+#define vb vector<bool>
+#define vii vector< vector<int> >
+#define mod 1e9 + 7
+#define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++)
+#define FORL(a, b, c) for (int(a) = (b); (a) <= (c); (a)++)
+#define FORR(a, b, c) for (int(a) = (b); (a) >= (c); (a)--)
+#define INF 1000000000000000003
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+
+class Compare {
+public:
+    bool operator() (int a, int b) {
+        return (a < b);
+    }
+};
+
+int main() {
+    FIO;
+    int arr[] = {10,15,20,13,6,90};
+    int n = sizeof(arr) / sizeof(int);
+    priority_queue<int, vector<int>, Compare > heap;
+    for(int x : arr) heap.push(x);
+    while(!heap.empty()) {
+        cout << heap.top() << nl;
+        heap.pop();
+    }
+    return 0;
+}
